287356f933ee813358feebfe26c7bb7b63d38bfe
praveenmarch22 <praveen.220302@gmail.com>
2025-01-23 21:31:27 +0530

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from "react-redux";
+import EditProfile from "./EditProfile";
+
+const Profile = () => {
+  const user = useSelector((store) => store.user);
+  console.log(user);
+  return (
+    user && (
+      <div>
+        <EditProfile user1={user} />
+      </div>
+    )
+  );
+};
+export default Profile;
